62966b39a0dac4b54ca05dc06fcb9683523323d7
Brotxer <145964719+Brotxer@users.noreply.github.com>
2024-10-30 15:00:49 +0200

    Add files via upload

---
+++ todo/precache-manifest.36d044f424d114ff9e37bb7578287cb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21a4ba861fb0b3a7e9bf",
+    "url": "/todo/static/css/main.75730263.chunk.css"
+  },
+  {
+    "revision": "21a4ba861fb0b3a7e9bf",
+    "url": "/todo/static/js/main.21a4ba86.chunk.js"
+  },
+  {
+    "revision": "e02cadb507357b924748",
+    "url": "/todo/static/css/1.f1395674.chunk.css"
+  },
+  {
+    "revision": "e02cadb507357b924748",
+    "url": "/todo/static/js/1.e02cadb5.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "37c9c774a3c30e7e610c71c41d130758",
+    "url": "/todo/index.html"
+  }
+];
---
+++ webApp/precache-manifest.465e2e08b47e34cd18403bcf8e84e426.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e27e63d2e79522c64874",
+    "url": "/webApp/static/css/main.5b7bfe33.chunk.css"
+  },
+  {
+    "revision": "e27e63d2e79522c64874",
+    "url": "/webApp/static/js/main.e27e63d2.chunk.js"
+  },
+  {
+    "revision": "ec395682d41d67ea01b7",
+    "url": "/webApp/static/css/1.bc26ed8e.chunk.css"
+  },
+  {
+    "revision": "ec395682d41d67ea01b7",
+    "url": "/webApp/static/js/1.ec395682.chunk.js"
+  },
+  {
+    "revision": "1fd6827edd8cc4d44399",
+    "url": "/webApp/static/js/runtime~main.1fd6827e.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/webApp/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/webApp/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "a55384425da11eb5c23a961272f9cffe",
+    "url": "/webApp/index.html"
+  }
+];
